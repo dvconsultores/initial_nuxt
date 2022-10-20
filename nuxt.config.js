260118dd1bfbc24ee53ae7b3d30465978a0def65
detextre4 <detextre4@gmail.com>
2022-10-20 13:47:26 -0400

    fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,8 +25,6 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      // <-- main -->
-      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
       // <-- icons-default -->
       { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
