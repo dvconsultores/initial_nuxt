8939ec5e50245cdc5a321d72569d66107b4df042
detextre4 <detextre4@gmail.com>
2023-05-14 20:21:17 -0400

    news

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="home" class="divcol">
-    <h1 class="hover_split maxsize_w">WHO ARE YOU?
-      <span class="mask"><span>WHO ARE YOU?</span></span>
-      <span class="mask"><span>WHO ARE YOU?</span></span>
-    </h1>
+    <div v-flex.column>
+      <h1 v-font:3,5="16" class="tcenter">Template detextre4</h1>
+      <div v-flex.space>
+        <h2 v-font:2,5="16">this is a example</h2>
+        <h2 v-font:2,5="16" class="tend">just a showcase</h2>
+      </div>
+    </div>
   </div>
 </template>
 
